df7d3a3e7946913ccf2775250ab0a8b3ff398b0e
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-11-01 14:36:42 -0500

    Changed FeatureFlags to Empty
    Hopefully this won't cause problems later

--- src/main/java/xreversef1ash/legacyduplicatormod/screens/ScreenDuplicatorRegistry.java
+++ src/main/java/xreversef1ash/legacyduplicatormod/screens/ScreenDuplicatorRegistry.java
@@ -2,7 +2,7 @@ package xreversef1ash.legacyduplicatormod.screens;
 
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.resource.featuretoggle.FeatureFlags;
+import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import xreversef1ash.legacyduplicatormod.LegacyDuplicatorMod;
@@ -12,7 +12,7 @@ public class ScreenDuplicatorRegistry {
             Registry.register(
                     Registries.SCREEN_HANDLER,
                     Identifier.of(LegacyDuplicatorMod.MOD_ID, "item_duplication"),
-                    new ScreenHandlerType<>(ItemDuplicatorScreenHandler::new, FeatureFlags.VANILLA_FEATURES)); // TODO: Do FeatureFlags need to be changed?
+                    new ScreenHandlerType<>(ItemDuplicatorScreenHandler::new, FeatureSet.empty()));
 
     public static void init() {}
 }
