182a468d0a4a3078b6e19070d74fac64bd70fbce
xEobardThawne <66641809+xEobardThawne@users.noreply.github.com>
2024-11-01 20:53:46 -0500

    Updated Item Blacklist

--- src/main/java/xreversef1ash/legacyduplicatormod/util/ItemDuplicatorItemList.java
+++ src/main/java/xreversef1ash/legacyduplicatormod/util/ItemDuplicatorItemList.java
@@ -1,6 +1,7 @@
 package xreversef1ash.legacyduplicatormod.util;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import xreversef1ash.legacyduplicatormod.blocks.DuplicatorBlockRegistry;
@@ -21,7 +22,26 @@ public class ItemDuplicatorItemList {
                     DuplicatorBlockRegistry.ITEM_DUPLICATOR_LVL_THREE.asItem(),
                     DuplicatorBlockRegistry.ITEM_DUPLICATOR_LVL_FOUR.asItem(),
                     DuplicatorItemRegistry.DUPLICATOR_STICK,
-                    DuplicatorItemRegistry.INFINIDUPLICATOR_STICK
+                    DuplicatorItemRegistry.INFINIDUPLICATOR_STICK,
+                    Items.SHULKER_BOX,
+                    Items.BLUE_SHULKER_BOX,
+                    Items.BLACK_SHULKER_BOX,
+                    Items.BROWN_SHULKER_BOX,
+                    Items.BLUE_SHULKER_BOX,
+                    Items.CYAN_SHULKER_BOX,
+                    Items.GRAY_SHULKER_BOX,
+                    Items.GREEN_SHULKER_BOX,
+                    Items.LIGHT_BLUE_SHULKER_BOX,
+                    Items.LIGHT_GRAY_SHULKER_BOX,
+                    Items.LIME_SHULKER_BOX,
+                    Items.MAGENTA_SHULKER_BOX,
+                    Items.ORANGE_SHULKER_BOX,
+                    Items.PINK_SHULKER_BOX,
+                    Items.PURPLE_SHULKER_BOX,
+                    Items.RED_SHULKER_BOX,
+                    Items.WHITE_SHULKER_BOX,
+                    Items.YELLOW_SHULKER_BOX,
+                    Items.BUNDLE
             )
     );
 
